fcf00e88c4d07647b03bcb7593ce10e2c2e14ecf
yeisonGutierrezquqo <ygutierrez@quqo.com>
2019-08-04 11:32:38 -0500

    agregar vista para Mostrar suscriptor

--- src/componentes/suscriptores/mostrarSuscriptor.js
+++ src/componentes/suscriptores/mostrarSuscriptor.js
@@ -1,7 +1,75 @@
 import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const MostarSuscriptor = () => {
-    return ( <h1>HOLA</h1> );
+import Spiner from '../layout/Spiner'
+
+
+const MostarSuscriptor = ({suscriptor}) => {
+
+    if (!suscriptor) return <Spiner />
+    console.log(suscriptor);
+    
+    return (
+        <div className="row">
+            <br /><br /><br /><br />
+            <div className="col-md-6 mb-4">
+                <br />
+                <Link to="/suscriptores" className="btn btn-secondary">
+                    <i className="fas fa-arrow-circle-left"></i>{' '}
+                    Volver al Listado
+                </Link>
+            </div>
+            <div className="col-md-6">
+            <br />
+                <Link to={`/suscriptores/EditarSuscriptor/${suscriptor.id}`} 
+                className="btn btn-primary float-right">
+                    <i className="fas fa-pencil-alt"></i>{' '}
+                    Editar Suscriptor
+                </Link>
+            </div>
+            <hr className="mx-5 w-100"/>
+            <div className="col-12">
+                <h2 className="mb-4">
+                    {suscriptor.nombre} {suscriptor.apellido}
+                </h2>
+
+                <p>
+                    <span className="font-weight-bold">
+                        Carrera:
+                    </span>
+                    {suscriptor.carrera}
+                </p>
+
+                <p>
+                    <span className="font-weight-bold">
+                        Código:
+                    </span>
+                    {suscriptor.codigo}
+                </p>
+
+            </div>
+        </div>
+        );
 }
- 
-export default MostarSuscriptor;
+
+MostarSuscriptor.protoTypes = {
+    firestore: PropTypes.object.isRequired
+}
+
+export default compose(
+    firestoreConnect(props => [
+        {
+            collection: 'suscriptores',
+            storeAs: 'suscriptor',
+            doc: props.match.params.id
+        }
+    ]),
+    connect(({firestore:{ordered}}, props) =>({
+        suscriptor: ordered.suscriptor && ordered.suscriptor[0]
+    }))
+    
+)(MostarSuscriptor);
--- src/componentes/suscriptores/nuevoSuscriptor.js
+++ src/componentes/suscriptores/nuevoSuscriptor.js
@@ -1,13 +1,130 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import { firestoreConnect } from 'react-redux-firebase'
+import PropTypes from 'prop-types'
 
 class NuevoSuscriptor extends Component {
+
+    state={
+        suscriptor: {
+            nombre: '',
+            apellido: '',
+            carrera: '',
+            codigo: ''
+        }
+    }
+
+    // Extraer los valores del input y colocarlos en el state
+    leerDato = e => {
+        let {suscriptor} = this.state
+        suscriptor[e.target.name] = e.target.value
+        this.setState({
+            suscriptor
+        })
+    } 
+
+    // agregar suscriptor
+    agregarSuscriptor = e => {
+        e.preventDefault();
+
+        // extraer los valores del state
+        const {suscriptor} = this.state
+        console.log(suscriptor);
+        
+
+        //extraer firestore de props
+        console.log(this.props.firestore);
+        const {firestore, history} = this.props
+
+        //Guardar en la vase de datos
+        firestore.add({collection: 'suscriptores'}, suscriptor)
+        .then(() => {
+            history.push('/suscriptores')
+        })
+    }
+
     render() {
         return (
-            <div>
-                NuevoSuscriptor
+            <div className="row">
+                <div className="col-12 mb-4">
+                    <br />
+                    <Link to={'/suscriptores'} className="btn btn-secondary">
+                        <i className="fas fa-arrow-circle-left"></i>{' '}
+                        Volver a Suscriptores
+                    </Link>
+                </div>
+                <div className="col-12">
+                    <h2>
+                        <i className="fas fa-user-plus">{' '}
+                            Nuevo Suscriptor
+                        </i>
+                    </h2>
+                </div> 
+                <div className="row ">
+                    <div className="col-md-12">
+                        <form onSubmit={this.agregarSuscriptor}>
+                            <div className="form-group">
+                                <label>Nombre:</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    name="nombre"
+                                    placeholder="Nombre del suscriptor"
+                                    required  
+                                    onChange={this.leerDato}
+                                    value={this.state.suscriptor.nombre}
+                                />
+                            </div>  
+
+                            <div className="form-group">
+                                <label>Apellido:</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    name="apellido"
+                                    placeholder="Apellido del suscriptor"
+                                    required  
+                                    onChange={this.leerDato}
+                                    value={this.state.suscriptor.apellido}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Carrera:</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    name="carrera"
+                                    placeholder="Carrera del suscriptor"
+                                    required  
+                                    onChange={this.leerDato}
+                                    value={this.state.suscriptor.carrera}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Codígo:</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    name="codigo"
+                                    placeholder="Codígo del suscriptor"
+                                    required  
+                                    onChange={this.leerDato}
+                                    value={this.state.suscriptor.codigo}
+                                />
+                            </div>
+
+                            <input type="submit" value="Agregar suscriptor" className="btn btn-success"/>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default NuevoSuscriptor;
+NuevoSuscriptor.propTypes ={
+    firestore: PropTypes.object.isRequired
+}
+
+export default firestoreConnect()(NuevoSuscriptor);

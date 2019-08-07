6e29f0ecde1d6acb86e633b4d6fd0c4bd9e159e9
yeisonGutierrezquqo <ygutierrez@quqo.com>
2019-08-07 01:27:54 -0500

    Comentar codigo

--- src/store.js
+++ src/store.js
@@ -48,6 +48,6 @@ const initialState = {}
 
 const store = createStoreWithFirebase(rootReducer, initialState, compose(
     reactReduxFirebase(firebase),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 export default store
